1b1fe91ea87d5f29fb44c354404c75278fa61c61
Petar Atanasov <petar.atanasov87@gmail.com>
2014-10-24 13:00:44 +0300

    Add week1 assignments

---
+++ week1/GestureImage/app/src/main/java/com/example/my/gestureimage/GestureImageActivity.java
@@ -0,0 +1,191 @@
+package com.example.my.gestureimage;
+
+import android.app.Activity;
+import android.graphics.PointF;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+
+public class GestureImageActivity extends Activity {
+
+    private ImageView imageView;
+    private View root;
+
+    private double startScale;
+    private float startRotation;
+    private double startTranslationX;
+    private double startTranslationY;
+
+    private PointF finger1 = new PointF(0, 0);
+    private PointF finger2 = new PointF(0, 0);
+    private PointF middle = new PointF(0, 0);
+
+    private double startScaleDistance;
+    private double currentScaleDistance;
+
+    private PointF startTranslationCenter = new PointF(0, 0);
+    private PointF currentTranslationCenter = new PointF(0, 0);
+
+    private PointF startRotationVector = new PointF(0, 0);
+    private PointF currentRotationVector = new PointF(0, 0);
+
+    private final static String TAG = "debug";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gesture_image);
+        imageView = (ImageView) findViewById(R.id.image);
+        root = findViewById(R.id.root);
+        root.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                updateFingersCoordinates(motionEvent);
+                updateCurrentTranslationCenter(motionEvent);
+
+                switch (motionEvent.getActionMasked()) {
+                    case MotionEvent.ACTION_DOWN:
+                        saveStartScale();
+                        saveStartTranslation();
+                        saveStartRotation();
+                        startTranslationCenter.set(motionEvent.getX(), motionEvent.getY());
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        updateTranslation();
+                        if (existsId(motionEvent, 0) && existsId(motionEvent, 1)) {
+                            updateScale();
+                            updateRotation();
+                        }
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        break;
+                    case MotionEvent.ACTION_POINTER_DOWN:
+                        if (existsId(motionEvent, 0) && existsId(motionEvent, 1)) {
+                            saveStartScale();
+                            saveStartTranslation();
+                            saveStartRotation();
+                            startTranslationCenter.set((finger1.x + finger2.x) / 2, (finger1.y + finger2.y) / 2);
+                            startScaleDistance = calculateDistance();
+                            startRotationVector.set(finger1.x - finger2.x, finger1.y - finger2.y);
+
+                            // The following if statement serves the case when the user puts a third finger,
+                            // removes the first two and then adds the first, because the case ACTION_DOWN is
+                            // bypassed.
+                        } else if (existsId(motionEvent, 0)) {
+                            saveStartTranslation();
+                            startTranslationCenter.set(finger1.x, finger1.y);
+                        }
+                        break;
+                    case MotionEvent.ACTION_POINTER_UP:
+                        int actionSourceId = motionEvent.getPointerId(motionEvent.getActionIndex());
+                        if (actionSourceId == 0 && existsId(motionEvent, 1)) {
+                            saveStartTranslation();
+                            startTranslationCenter.set(finger2.x, finger2.y);
+                            currentTranslationCenter.set(finger2.x, finger2.y);
+                        }
+                        if (actionSourceId == 1 && existsId(motionEvent, 0)) {
+                            saveStartTranslation();
+                            startTranslationCenter.set(finger1.x, finger1.y);
+                            currentTranslationCenter.set(finger1.x, finger1.y);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void saveStartRotation() {
+        startRotation = imageView.getRotation();
+    }
+
+    private void updateRotation() {
+        currentRotationVector.set(finger1.x - finger2.x, finger1.y - finger2.y);
+        double angleStart = Math.atan2(startRotationVector.x, startRotationVector.y);
+        double angleEnd = Math.atan2(currentRotationVector.x, currentRotationVector.y);
+        float angle = (float) (angleStart - angleEnd);
+        imageView.setRotation(angle * (180.0f / (float) Math.PI) + startRotation);
+    }
+
+    private void updateScale() {
+        currentScaleDistance = calculateDistance();
+        imageView.setScaleX((float) ((startScale * (currentScaleDistance / startScaleDistance))));
+        imageView.setScaleY((float) ((startScale * (currentScaleDistance / startScaleDistance))));
+    }
+
+    private void updateTranslation() {
+        imageView.setTranslationX((float) (startTranslationX + currentTranslationCenter.x - startTranslationCenter.x));
+        imageView.setTranslationY((float) (startTranslationY + currentTranslationCenter.y - startTranslationCenter.y));
+    }
+
+    private void saveStartScale() {
+        startScale = imageView.getScaleX();
+    }
+
+    private void saveStartTranslation() {
+        startTranslationX = imageView.getTranslationX();
+        startTranslationY = imageView.getTranslationY();
+    }
+
+    private void updateFingersCoordinates(MotionEvent motionEvent) {
+        for (int i = 0; i < motionEvent.getPointerCount(); i++) {
+            if (motionEvent.getPointerId(i) == 0) {
+                finger1.set(motionEvent.getX(i), motionEvent.getY(i));
+            } else if (motionEvent.getPointerId(i) == 1) {
+                finger2.set(motionEvent.getX(i), motionEvent.getY(i));
+            }
+        }
+
+        if (existsId(motionEvent, 0) && existsId(motionEvent, 1)) {
+            middle.set((finger1.x + finger2.x) / 2, (finger1.y + finger2.y) / 2);
+        } else if (existsId(motionEvent, 0)) {
+            middle.set(finger1.x, finger1.y);
+        } else if (existsId(motionEvent, 1)) {
+            middle.set(finger2.x, finger2.y);
+        }
+    }
+
+    private void updateCurrentTranslationCenter(MotionEvent motionEvent) {
+        boolean finger1Exists = existsId(motionEvent, 0);
+        boolean finger2Exists = existsId(motionEvent, 1);
+
+        if (finger1Exists && finger2Exists) {
+            currentTranslationCenter.set((finger1.x + finger2.x) / 2, (finger1.y + finger2.y) / 2);
+        } else if (finger1Exists) {
+            currentTranslationCenter.set(finger1.x, finger1.y);
+        } else if (finger2Exists) {
+            currentTranslationCenter.set(finger2.x, finger2.y);
+        }
+    }
+
+    private boolean existsId(MotionEvent motionEvent, int id) {
+        for (int i = 0; i < motionEvent.getPointerCount(); i++) {
+            if (motionEvent.getPointerId(i) == id) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private double calculateDistance() {
+        float x1 = finger1.x;
+        float x2 = finger2.x;
+        float y1 = finger1.y;
+        float y2 = finger2.y;
+        return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    }
+
+    private float deltaX() {
+        return currentTranslationCenter.x - startTranslationCenter.x;
+    }
+
+    private float deltaY() {
+        return currentTranslationCenter.y - startTranslationCenter.y;
+    }
+}
---
+++ week1/PuzzleGame/app/src/main/java/com/example/my/puzzlegame/MyActivity.java
@@ -0,0 +1,129 @@
+package com.example.my.puzzlegame;
+
+import android.app.Activity;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+public class MyActivity extends Activity implements View.OnDragListener, View.OnTouchListener {
+
+    private final String INDEX_ARRAY = "index_array";
+    private final int columns = 4;
+    private final int rows = 4;
+    private List<Integer> correspondingImageIndexex = new ArrayList<Integer>();
+    TypedArray images;
+    private LinearLayout root;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_my);
+        images = getResources().obtainTypedArray(R.array.images);
+        images.recycle();
+
+        if (savedInstanceState == null) {
+            for (int i = 0; i < images.length(); i++) {
+                correspondingImageIndexex.add(i);
+            }
+            randomize();
+        } else {
+            correspondingImageIndexex = savedInstanceState.getIntegerArrayList(INDEX_ARRAY);
+        }
+
+        root = (LinearLayout) findViewById(R.id.root);
+        LinearLayout row = new LinearLayout(this);
+        for (int i = 0; i < images.length(); i++) {
+            if (i % columns == 0) {
+                if (i != 0) {
+                    root.addView(row);
+                }
+                row = new LinearLayout(this);
+                row.setLayoutParams(new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        0,
+                        1f
+                ));
+                row.setOrientation(LinearLayout.HORIZONTAL);
+            }
+            ImageView view = new ImageView(this);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 1f);
+            view.setLayoutParams(params);
+            view.setAdjustViewBounds(true);
+            view.setPadding(2, 2, 2, 2);
+            view.setImageDrawable(images.getDrawable(correspondingImageIndexex.get(i)));
+            view.setAdjustViewBounds(true);
+            view.setOnTouchListener(this);
+            view.setOnDragListener(this);
+            view.setTag(new Integer(i));
+            row.addView(view);
+        }
+        root.addView(row);
+    }
+
+    private void randomize() {
+        int size = correspondingImageIndexex.size();
+        Random rand = new Random();
+        for (int i = 0; i < size; i++) {
+            int idx1 = rand.nextInt(size);
+            int idx2 = rand.nextInt(size);
+            int val1 = correspondingImageIndexex.get(idx1);
+            correspondingImageIndexex.set(idx1, correspondingImageIndexex.get(idx2));
+            correspondingImageIndexex.set(idx2, val1);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putIntegerArrayList(INDEX_ARRAY, (ArrayList<Integer>) correspondingImageIndexex);
+    }
+
+    @Override
+    public boolean onDrag(View view, DragEvent dragEvent) {
+        if (dragEvent.getAction() == DragEvent.ACTION_DROP) {
+            ImageView source = (ImageView) dragEvent.getLocalState();
+            Drawable sourceImage = source.getDrawable();
+            source.setImageDrawable(((ImageView) view).getDrawable());
+            ((ImageView) view).setImageDrawable(sourceImage);
+            Integer sourceId = (Integer) source.getTag();
+            Integer destinationId = (Integer) view.getTag();
+            int sourceImageId = correspondingImageIndexex.get(sourceId);
+            correspondingImageIndexex.set(sourceId, correspondingImageIndexex.get(destinationId));
+            correspondingImageIndexex.set(destinationId, sourceImageId);
+            if(isArranged())  {
+                Toast toast = Toast.makeText(this, "Congratiolations! You have completed the puzzle.", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        }
+        return true;
+    }
+
+    private boolean isArranged() {
+        for (int i = 0; i < correspondingImageIndexex.size(); i++) {
+            if (correspondingImageIndexex.get(i) != i) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        view.startDrag(null, new View.DragShadowBuilder(view), view, 0);
+        return false;
+    }
+}
